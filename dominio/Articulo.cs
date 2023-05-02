038f38d159b43068829826b1eb8b5bf250c4b067
Gnieva94 <gus_94@hotmail.com.ar>
2023-05-02 19:32:13 -0300

    Primera subida de proyecto (Realizado entre los 3)

---
+++ dominio/Articulo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Articulo
+    {
+        /*
+         * Los datos mínimos con los que deberá contar el artículo son los siguientes:
+
+            Código de artículo.
+            Nombre.
+            Descripción.
+            Marca (seleccionable de una lista desplegable).
+            Categoría (seleccionable de una lista desplegable.
+            Imagen.
+            Precio.
+         */
+        public int CodigoArticulo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Marca Marca { get; set; }
+        public Categoria Categoria { get; set; }
+        public string Imagen { get; set; }
+        public decimal Precio { get; set; }
+
+    }
+}
