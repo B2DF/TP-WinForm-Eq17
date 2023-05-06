b90da9384338a5fda5058fca5931578c554bea79
Gnieva94 <gus_94@hotmail.com.ar>
2023-05-06 10:40:44 -0300

    Agregados clases de Negocio y Acceso a datos

---
+++ negocio/ArticuloNegocio.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    public class ArticuloNegocio
+    {
+        public List<Articulo> listar()
+        {
+            List<Articulo> listaArticulos = new List<Articulo>();
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                //datos.setearConsulta("select A.Id, A.Codigo, A.Nombre, A.Descripcion, A.Precio,M.Id as IdMarca, M.Descripcion as Marca,C.Id as IdCategoria, C.Descripcion as Categoria from ARTICULOS as A inner join MARCAS as M on A.IdMarca = M.Id inner join CATEGORIAS as C on A.IdCategoria = C.Id");
+                //datos.setearConsulta("SELECT Id,Codigo,Nombre,Descripcion FROM ARTICULOS");
+                datos.setearConsulta("SELECT A.Id AId,Codigo,Nombre,A.Descripcion ADes, C.Descripcion CDes, M.Descripcion MDes, Precio FROM ARTICULOS A, CATEGORIAS C, MARCAS M WHERE C.Id=A.IdCategoria AND M.Id=A.IdMarca\r\n");
+                datos.ejecutarLectura();
+                while (datos.Lector.Read())
+                {
+                    Articulo aux = new Articulo();
+                    aux.Id = (int)datos.Lector["AId"];
+                    aux.Codigo = (string)datos.Lector["Codigo"];
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+                    aux.Descripcion = (string)datos.Lector["ADes"];
+                    aux.Categoria = new Categoria();
+                    //aux.Categoria.Id = (int)datos.Lector["IdCategoria"];
+                    aux.Categoria.Descripcion = (string)datos.Lector["CDes"];
+                    aux.Marca = new Marca();
+                    //aux.Marca.Id = (int)datos.Lector["IdMarca"];
+                    aux.Marca.Descripcion = (string)datos.Lector["MDes"];
+                    aux.Precio = (decimal)datos.Lector["Precio"];
+                    listaArticulos.Add(aux);
+                }
+                return listaArticulos;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerraConexion();
+            }
+        }
+    }
+}
---
+++ negocio/ImagenesNegocio.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    public class ImagenesNegocio
+    {
+        public List<Imagenes> listar()
+        {
+            List<Imagenes> listaImagenes = new List<Imagenes>();
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("SELECT Id, IdArticulo, ImagenUrl FROM IMAGENES");
+                datos.ejecutarLectura();
+                while (datos.Lector.Read())
+                {
+                    Imagenes aux = new Imagenes();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.IdArticulo = (int)datos.Lector["IdArticulo"];
+                    aux.ImagenUrl = (string)datos.Lector["ImagenUrl"];
+                    listaImagenes.Add(aux);
+                }
+                return listaImagenes;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datos.cerraConexion();
+            }
+        }
+    }
+}
