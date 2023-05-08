6e31a714be8297af4af4d1d4497a46661da60949
Gnieva94 <gus_94@hotmail.com.ar>
2023-05-08 17:08:45 -0300

    Added frmOtros.

---
+++ TP-WinForm-Eq17/frmOtros.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace TP_WinForm_Eq17
+{
+    partial class frmOtros
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnLimpiarCampo = new System.Windows.Forms.Button();
+            this.lblContImg = new System.Windows.Forms.Label();
+            this.btnSiguiente = new System.Windows.Forms.Button();
+            this.btnAtras = new System.Windows.Forms.Button();
+            this.lblContTotal = new System.Windows.Forms.Label();
+            this.pbxCheckError = new System.Windows.Forms.PictureBox();
+            this.gpbxArticulo = new System.Windows.Forms.GroupBox();
+            this.tbxNombre = new System.Windows.Forms.TextBox();
+            this.lblIdArticulo = new System.Windows.Forms.Label();
+            this.tbxId = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.pbxOtros = new System.Windows.Forms.PictureBox();
+            this.tbxSource = new System.Windows.Forms.TextBox();
+            this.lblSource = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCheckError)).BeginInit();
+            this.gpbxArticulo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxOtros)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(105, 463);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(86, 23);
+            this.btnModificar.TabIndex = 23;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnLimpiarCampo
+            // 
+            this.btnLimpiarCampo.Location = new System.Drawing.Point(223, 22);
+            this.btnLimpiarCampo.Name = "btnLimpiarCampo";
+            this.btnLimpiarCampo.Size = new System.Drawing.Size(21, 20);
+            this.btnLimpiarCampo.TabIndex = 13;
+            this.btnLimpiarCampo.UseVisualStyleBackColor = true;
+            this.btnLimpiarCampo.Click += new System.EventHandler(this.btnLimpiarCampo_Click);
+            // 
+            // lblContImg
+            // 
+            this.lblContImg.AutoSize = true;
+            this.lblContImg.Location = new System.Drawing.Point(137, 413);
+            this.lblContImg.Name = "lblContImg";
+            this.lblContImg.Size = new System.Drawing.Size(19, 13);
+            this.lblContImg.TabIndex = 22;
+            this.lblContImg.Text = "aa";
+            // 
+            // btnSiguiente
+            // 
+            this.btnSiguiente.Location = new System.Drawing.Point(197, 408);
+            this.btnSiguiente.Name = "btnSiguiente";
+            this.btnSiguiente.Size = new System.Drawing.Size(75, 23);
+            this.btnSiguiente.TabIndex = 17;
+            this.btnSiguiente.Text = "Siguiente";
+            this.btnSiguiente.UseVisualStyleBackColor = true;
+            this.btnSiguiente.Click += new System.EventHandler(this.btnSiguiente_Click);
+            // 
+            // btnAtras
+            // 
+            this.btnAtras.Location = new System.Drawing.Point(23, 408);
+            this.btnAtras.Name = "btnAtras";
+            this.btnAtras.Size = new System.Drawing.Size(75, 23);
+            this.btnAtras.TabIndex = 15;
+            this.btnAtras.Text = "Atrás";
+            this.btnAtras.UseVisualStyleBackColor = true;
+            this.btnAtras.Click += new System.EventHandler(this.btnAtras_Click);
+            // 
+            // lblContTotal
+            // 
+            this.lblContTotal.AutoSize = true;
+            this.lblContTotal.Location = new System.Drawing.Point(20, 444);
+            this.lblContTotal.Name = "lblContTotal";
+            this.lblContTotal.Size = new System.Drawing.Size(19, 13);
+            this.lblContTotal.TabIndex = 21;
+            this.lblContTotal.Text = "aa";
+            // 
+            // pbxCheckError
+            // 
+            this.pbxCheckError.Location = new System.Drawing.Point(250, 22);
+            this.pbxCheckError.Name = "pbxCheckError";
+            this.pbxCheckError.Size = new System.Drawing.Size(19, 20);
+            this.pbxCheckError.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxCheckError.TabIndex = 19;
+            this.pbxCheckError.TabStop = false;
+            // 
+            // gpbxArticulo
+            // 
+            this.gpbxArticulo.Controls.Add(this.tbxNombre);
+            this.gpbxArticulo.Controls.Add(this.lblIdArticulo);
+            this.gpbxArticulo.Controls.Add(this.tbxId);
+            this.gpbxArticulo.Controls.Add(this.lblNombre);
+            this.gpbxArticulo.Location = new System.Drawing.Point(23, 57);
+            this.gpbxArticulo.Name = "gpbxArticulo";
+            this.gpbxArticulo.Size = new System.Drawing.Size(249, 100);
+            this.gpbxArticulo.TabIndex = 14;
+            this.gpbxArticulo.TabStop = false;
+            this.gpbxArticulo.Text = "Articulo";
+            // 
+            // tbxNombre
+            // 
+            this.tbxNombre.Location = new System.Drawing.Point(78, 31);
+            this.tbxNombre.Name = "tbxNombre";
+            this.tbxNombre.ReadOnly = true;
+            this.tbxNombre.Size = new System.Drawing.Size(140, 20);
+            this.tbxNombre.TabIndex = 0;
+            this.tbxNombre.TabStop = false;
+            // 
+            // lblIdArticulo
+            // 
+            this.lblIdArticulo.AutoSize = true;
+            this.lblIdArticulo.Location = new System.Drawing.Point(28, 57);
+            this.lblIdArticulo.Name = "lblIdArticulo";
+            this.lblIdArticulo.Size = new System.Drawing.Size(21, 13);
+            this.lblIdArticulo.TabIndex = 7;
+            this.lblIdArticulo.Text = "ID:";
+            // 
+            // tbxId
+            // 
+            this.tbxId.Location = new System.Drawing.Point(78, 57);
+            this.tbxId.Name = "tbxId";
+            this.tbxId.ReadOnly = true;
+            this.tbxId.Size = new System.Drawing.Size(71, 20);
+            this.tbxId.TabIndex = 1;
+            this.tbxId.TabStop = false;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(28, 31);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(47, 13);
+            this.lblNombre.TabIndex = 5;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(197, 463);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 20;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(23, 463);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 18;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // pbxOtros
+            // 
+            this.pbxOtros.Location = new System.Drawing.Point(23, 163);
+            this.pbxOtros.Name = "pbxOtros";
+            this.pbxOtros.Size = new System.Drawing.Size(249, 239);
+            this.pbxOtros.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxOtros.TabIndex = 16;
+            this.pbxOtros.TabStop = false;
+            // 
+            // tbxSource
+            // 
+            this.tbxSource.Location = new System.Drawing.Point(76, 22);
+            this.tbxSource.Name = "tbxSource";
+            this.tbxSource.Size = new System.Drawing.Size(140, 20);
+            this.tbxSource.TabIndex = 11;
+            this.tbxSource.Leave += new System.EventHandler(this.tbxSource_Leave);
+            // 
+            // lblSource
+            // 
+            this.lblSource.AutoSize = true;
+            this.lblSource.Location = new System.Drawing.Point(26, 25);
+            this.lblSource.Name = "lblSource";
+            this.lblSource.Size = new System.Drawing.Size(44, 13);
+            this.lblSource.TabIndex = 12;
+            this.lblSource.Text = "Source:";
+            // 
+            // frmOtros
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 508);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnLimpiarCampo);
+            this.Controls.Add(this.lblContImg);
+            this.Controls.Add(this.btnSiguiente);
+            this.Controls.Add(this.btnAtras);
+            this.Controls.Add(this.lblContTotal);
+            this.Controls.Add(this.pbxCheckError);
+            this.Controls.Add(this.gpbxArticulo);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.pbxOtros);
+            this.Controls.Add(this.tbxSource);
+            this.Controls.Add(this.lblSource);
+            this.Name = "frmOtros";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmOtros";
+            this.Load += new System.EventHandler(this.frmOtros_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCheckError)).EndInit();
+            this.gpbxArticulo.ResumeLayout(false);
+            this.gpbxArticulo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxOtros)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnLimpiarCampo;
+        private System.Windows.Forms.Label lblContImg;
+        private System.Windows.Forms.Button btnSiguiente;
+        private System.Windows.Forms.Button btnAtras;
+        private System.Windows.Forms.Label lblContTotal;
+        private System.Windows.Forms.PictureBox pbxCheckError;
+        private System.Windows.Forms.GroupBox gpbxArticulo;
+        private System.Windows.Forms.TextBox tbxNombre;
+        private System.Windows.Forms.Label lblIdArticulo;
+        private System.Windows.Forms.TextBox tbxId;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.PictureBox pbxOtros;
+        private System.Windows.Forms.TextBox tbxSource;
+        private System.Windows.Forms.Label lblSource;
+    }
+}
---
+++ TP-WinForm-Eq17/frmOtros.cs
@@ -0,0 +1,252 @@
+﻿using dominio;
+using negocio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP_WinForm_Eq17
+{
+    public partial class frmOtros : Form
+    {
+        private Articulo articulo = null;
+        private List<Imagenes> imagenes; //TODAS LAS IMAGENES DE LA DB
+        private List<Imagenes> listaImagenesAlta = new List<Imagenes>();//Lista cuando no hay articulo
+        private List<Imagenes> listaImgFiltradas = new List<Imagenes>();//Lista cuando hay articulo
+        private List<Imagenes> listaModificar = new List<Imagenes>();//Lista cuando hay articulo y modificamos, para retornar.
+        private List<Imagenes> listaAgregarImgArticulosExistentes = new List<Imagenes>();//Lista agregar imagenes a articulos existentes, para retornar
+        private int contador = 0;
+        private int currentImg = 1;
+        public frmOtros()
+        {
+            InitializeComponent();
+            Text = "Agregar";
+        }
+        public frmOtros(Articulo articulo)
+        {
+            InitializeComponent();
+            this.articulo = articulo;
+            Text = "Modificar/Agregar";
+        }
+
+        private void frmOtros_Load(object sender, EventArgs e)
+        {
+            lblContTotal.Text = "";
+            ImagenesNegocio imagenesNegocio = new ImagenesNegocio();
+            try
+            {
+                if (articulo == null)
+                {
+                    gpbxArticulo.Visible = false;
+                    lblContTotal.Text = "Imagenes: " + contador;
+                    lblContImg.Text = "" + contador + "/" + listaImagenesAlta.Count;
+                }
+                else
+                {
+                    gpbxArticulo.Visible = true;
+                    imagenes = imagenesNegocio.listar();
+                    listaImgFiltradas = imagenes.FindAll(x => x.IdArticulo == articulo.Id);
+                    if (listaImgFiltradas.Count == 0)
+                        cargarImagen("");
+                    else
+                        cargarImagen(listaImgFiltradas[0].ImagenUrl);
+                    contador = listaImgFiltradas.Count;
+                    tbxNombre.Text = articulo.Nombre;
+                    tbxId.Text = articulo.Id.ToString();
+                    if (listaImgFiltradas.Count > 0)
+                    {
+                        tbxSource.Text = listaImgFiltradas[currentImg - 1].ImagenUrl;
+                    }
+                    lblContImg.Text = "" + currentImg + "/" + listaImgFiltradas.Count;
+                    lblContTotal.Text = "Imagenes: " + contador;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void tbxSource_Leave(object sender, EventArgs e)
+        {
+            cargarImagen(tbxSource.Text);
+        }
+        private void cargarImagen(string url)
+        {
+            try
+            {
+                pbxOtros.Load(url);
+            }
+            catch (Exception ex)
+            {
+                pbxOtros.Load("https://www.campana.gob.ar/wp-content/uploads/2022/05/placeholder-1.png");
+            }
+        }
+        private void cargarImagenChecked(string url)
+        {
+            try
+            {
+                pbxCheckError.Load(url);
+            }
+            catch (Exception ex)
+            {
+
+                pbxCheckError.Visible = false;
+            }
+        }
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Imagenes imagen = new Imagenes();
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                if (articulo == null)
+                {
+                    if (tbxSource.Text == "")
+                    {
+                        cargarImagenChecked("https://e7.pngegg.com/pngimages/943/637/png-clipart-stop-sign-no-symbol-warning-sign-red-block-sign-s-angle-text.png");
+                        return;
+                    }
+                    else
+                    {
+                        cargarImagenChecked("https://e7.pngegg.com/pngimages/944/793/png-clipart-computer-icons-check-mark-presentation-symbol-check-list-miscellaneous-angle.png");
+                        datos.setearConsulta("SELECT MAX(Id) maxId FROM ARTICULOS");
+                        imagen.IdArticulo = (int)datos.ejecturarScalar() + 1;
+                        imagen.ImagenUrl = tbxSource.Text;
+                    }
+                    listaImagenesAlta.Add(imagen);
+                    lblContImg.Text = "" + currentImg + "/" + listaImagenesAlta.Count;
+                }
+                else
+                {
+                    if (tbxSource.Text == "")
+                    {
+                        cargarImagenChecked("https://e7.pngegg.com/pngimages/943/637/png-clipart-stop-sign-no-symbol-warning-sign-red-block-sign-s-angle-text.png");
+                        return;
+                    }
+                    else
+                    {
+                        cargarImagenChecked("https://e7.pngegg.com/pngimages/944/793/png-clipart-computer-icons-check-mark-presentation-symbol-check-list-miscellaneous-angle.png");
+                        imagen.ImagenUrl = tbxSource.Text;
+                        imagen.IdArticulo = articulo.Id;
+                    }
+                    listaImgFiltradas.Add(imagen);
+                    listaAgregarImgArticulosExistentes.Add(imagen);
+                    lblContImg.Text = "" + currentImg + "/" + listaImgFiltradas.Count;
+                }
+                MessageBox.Show("Agregado correcto. Puede seguir cargando imagenes.");
+                contador++;
+                lblContTotal.Text = "Imagenes: " + contador;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        public List<Imagenes> retornarListaImagenesAlta()
+        {
+            return listaImagenesAlta;
+        }
+        public List<Imagenes> retornarListaImgModificar()
+        {
+            return listaModificar;
+        }
+        public List<Imagenes> retornarListaAgregarImgArtExistenes()
+        {
+            return listaAgregarImgArticulosExistentes;
+        }
+
+        private void btnAtras_Click(object sender, EventArgs e)
+        {
+            if (articulo == null)
+            {
+                if (listaImagenesAlta.Count > 1)
+                {
+                    if (currentImg > 1)
+                    {
+                        currentImg--;
+                    }
+                    cargarImagen(listaImagenesAlta[currentImg - 1].ImagenUrl);
+                    lblContImg.Text = "" + currentImg + "/" + listaImagenesAlta.Count;
+                    tbxSource.Text = listaImagenesAlta[currentImg - 1].ImagenUrl;
+                }
+            }
+            else
+            {
+                if (listaImgFiltradas.Count > 1)
+                {
+                    if (currentImg > 1)
+                    {
+                        currentImg--;
+                    }
+                    cargarImagen(listaImgFiltradas[currentImg - 1].ImagenUrl);
+                    lblContImg.Text = "" + currentImg + "/" + listaImgFiltradas.Count;
+                    tbxSource.Text = listaImgFiltradas[currentImg - 1].ImagenUrl;
+                }
+            }
+        }
+
+        private void btnSiguiente_Click(object sender, EventArgs e)
+        {
+            if (articulo == null)
+            {
+                if (listaImagenesAlta.Count > 1)
+                {
+                    if (currentImg < listaImagenesAlta.Count)
+                    {
+                        currentImg++;
+                    }
+                    cargarImagen(listaImagenesAlta[currentImg - 1].ImagenUrl);
+                    lblContImg.Text = "" + currentImg + "/" + listaImagenesAlta.Count;
+                    tbxSource.Text = listaImagenesAlta[currentImg - 1].ImagenUrl;
+                }
+            }
+            else
+            {
+                if (listaImgFiltradas.Count > 1)
+                {
+                    if (currentImg < listaImgFiltradas.Count)
+                    {
+                        currentImg++;
+                    }
+                    cargarImagen(listaImgFiltradas[currentImg - 1].ImagenUrl);
+                    lblContImg.Text = "" + currentImg + "/" + listaImgFiltradas.Count;
+                    tbxSource.Text = listaImgFiltradas[currentImg - 1].ImagenUrl;
+                }
+            }
+        }
+
+        private void btnLimpiarCampo_Click(object sender, EventArgs e)
+        {
+            tbxSource.Text = "";
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            if (articulo != null && listaImgFiltradas.Count > 0)
+            {
+                listaImgFiltradas[currentImg - 1].ImagenUrl = tbxSource.Text;
+                listaModificar.Add(listaImgFiltradas[currentImg - 1]);
+            }
+            else
+            {
+                if (listaImagenesAlta.Count == 0)
+                {
+                    MessageBox.Show("No existe una imagen para el articulo, Agreguelo.");
+                }
+                else
+                    listaImagenesAlta[currentImg - 1].ImagenUrl = tbxSource.Text;
+            }
+        }
+    }
+}
