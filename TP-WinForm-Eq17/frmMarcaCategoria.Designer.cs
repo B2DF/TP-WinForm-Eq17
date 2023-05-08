d1fb1bc649caaee8f24827faca697a37352df90a
caroccucurullo <caroccucurullo@gmail.com>
2023-05-08 19:51:01 -0300

    Formulario Marca Categoria

---
+++ TP-WinForm-Eq17/frmMarcaCategoria.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace TP_WinForm_Eq17
+{
+    partial class frmMarcaCategoria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.tbxNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(232, 127);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 9;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(132, 127);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 8;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(34, 127);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 7;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // tbxNombre
+            // 
+            this.tbxNombre.Location = new System.Drawing.Point(138, 35);
+            this.tbxNombre.Name = "tbxNombre";
+            this.tbxNombre.Size = new System.Drawing.Size(160, 20);
+            this.tbxNombre.TabIndex = 6;
+            this.tbxNombre.TextChanged += new System.EventHandler(this.tbxNombre_TextChanged);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(76, 38);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(47, 13);
+            this.lblNombre.TabIndex = 5;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // frmMarcaCategoria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 195);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.tbxNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Name = "frmMarcaCategoria";
+            this.Text = "frmMarcaCategoria";
+            this.Load += new System.EventHandler(this.frmMarcaCategoria_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.TextBox tbxNombre;
+        private System.Windows.Forms.Label lblNombre;
+    }
+}
---
+++ TP-WinForm-Eq17/frmMarcaCategoria.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using dominio;
+using negocio;
+
+namespace TP_WinForm_Eq17
+{
+    public partial class frmMarcaCategoria : Form
+    {
+        string tag;
+        private List<Categoria> listaCategoria;
+        private List<Marca> listaMarca;
+
+        public frmMarcaCategoria()
+        {
+            InitializeComponent();
+        }
+        public frmMarcaCategoria(string tag)
+        {
+            InitializeComponent();
+            this.tag = tag;
+        }
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmMarcaCategoria_Load(object sender, EventArgs e)
+        {
+            if(tag == "Marca")
+            {
+                Marca marca = new Marca();
+                MarcaNegocio marcaNegocio = new MarcaNegocio();
+                //listaMarca = marcaNegocio.listar();
+                lblNombre.Text = "Marca:";
+
+            }
+            else if(tag == "Categoria")
+            {
+                Categoria categoria = new Categoria();
+                CategoriaNegocio categoriaNegocio = new CategoriaNegocio();
+                //listaCategoria= categoriaNegocio.listar();
+                lblNombre.Text = "Categoria";
+            }
+        }
+
+      
+    }
+}
